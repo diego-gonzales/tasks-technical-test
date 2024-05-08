41982e2e2912c70fc393e53377dcfea4f0a32542
Diego Gonzales <diegoegj14@gmail.com>
2024-05-08 18:44:13 -0500

    add eslint and prettier config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styles: [],
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet],
+	templateUrl: './app.component.html',
+	styles: []
 })
 export class AppComponent {
-  title = 'tasks-app';
+	title: string = 'tasks-app';
 }
