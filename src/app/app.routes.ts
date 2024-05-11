27bc92c45575d7a1e820e7a58a8cc70a0cb2921c
Diego Gonzales <diegoegj14@gmail.com>
2024-05-11 11:01:16 -0500

    add login and tasks components, main routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'login',
+		loadComponent: () => import('@pages/login/login.component'),
+		title: 'Login'
+	},
+	{
+		path: 'tasks',
+		loadComponent: () => import('@pages/tasks/tasks.component'),
+		title: 'Tasks'
+	},
+	{
+		path: '',
+		redirectTo: 'tasks',
+		pathMatch: 'full'
+	},
+	{
+		path: '**',
+		redirectTo: 'tasks'
+	}
+];
